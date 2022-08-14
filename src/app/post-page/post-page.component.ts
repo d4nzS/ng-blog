2c9885c759002d07972b350d767c25be05979415
d4nzS <dzianis.karym@gmail.com>
2022-08-14 19:37:06 +0300

    finish post-page

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-page',
-  templateUrl: './post-page.component.html'
+  templateUrl: './post-page.component.html',
+  styleUrls: ['./post-page.component.scss']
 })
 export class PostPageComponent {
 
