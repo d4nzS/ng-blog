420949f0fc71d0b931ad445bb7d9dda943bddbf6
d4nzS <dzianis.karym@gmail.com>
2022-08-14 19:49:36 +0300

    finish admin-page

--- src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
 })
 export class AdminLayoutComponent {
+  constructor(private router: Router,
+              private route: ActivatedRoute) {
+  }
 
+  public logout(event: MouseEvent) {
+    event.preventDefault();
+
+    this.router.navigate(['login'], { relativeTo: this.route });
+  }
 }
 
