092748d2fec8cb3504a839715abaf6676965c236
d4nzS <dzianis.karym@gmail.com>
2022-08-14 19:22:28 +0300

    finish home-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainLayoutComponent } from "./shared/components/main-layout.component";
+import { MainLayoutComponent } from "./shared/components/main-layout/main-layout.component";
 import { HomePageComponent } from "./home-page/home-page.component";
 import { PostPageComponent } from './post-page/post-page.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
     HomePageComponent,
-    PostPageComponent
+    PostPageComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,4 +23,5 @@ import { PostPageComponent } from './post-page/post-page.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
