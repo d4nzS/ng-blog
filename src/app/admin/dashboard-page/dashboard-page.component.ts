de523486161d58296cdc2e38d040f9611ca77b47
d4nzS <dzianis.karym@gmail.com>
2022-08-18 00:46:34 +0300

    work with dashboard-page

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from "rxjs";
+
+import { PostService } from "../../shared/services/post.service";
+import { Post } from "../../shared/interfaces";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  public posts: Post[] = [];
 
-  constructor() { }
+  private sub: Subscription;
 
-  ngOnInit(): void {
+  constructor(private postService: PostService) {
   }
 
+  ngOnInit(): void {
+    this.sub = this.postService.getAll().subscribe(posts => this.posts = posts);
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  remove(id: string): void {
+
+  }
 }
--- src/app/shared/services/post.service.ts
+++ src/app/shared/services/post.service.ts
@@ -20,4 +20,16 @@ export class PostService {
         date: new Date(post.date)
       })));
   }
+
+  public getAll(): Observable<Post[]> {
+    return this.http.get(`${environment.fbDbUrl}/posts.json`)
+      .pipe(map((response: {[key: string]: any}) => Object
+        .keys(response)
+        .map(key => ({
+          ...response[key],
+          id: key,
+          date: new Date(response[key].date)
+        }))
+      ))
+  }
 }
